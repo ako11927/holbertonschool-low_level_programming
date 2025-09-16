a689f0f04da141361ee8a450caadee54760df1e5
ako11927 <11927@holbertonstudents.com>
2025-09-16 20:45:08 +0000

    more_malloc_free: tasks 0-3

--- more_malloc_free/2-calloc.c
+++ more_malloc_free/2-calloc.c
@@ -13,12 +13,27 @@
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
 	size_t total;
-	void *p;
-	unsigned char *q;
+	void *ptr;
+	unsigned char *p;
 	size_t i;
 
 	if (nmemb == 0 || size == 0)
 		return (NULL);
 
-	/* overflow guard: nmemb * size must fit in size_t */
-	if ((size
+	/* overflow guard: (size_t)nmemb * (size_t)size must not wrap */
+	if ((size_t)nmemb > ((size_t)-1) / (size_t)size)
+		return (NULL);
+
+	total = (size_t)nmemb * (size_t)size;
+
+	ptr = malloc(total);
+	if (ptr == NULL)
+		return (NULL);
+
+	/* zero manually (memset not allowed) */
+	p = (unsigned char *)ptr;
+	for (i = 0; i < total; i++)
+		p[i] = 0;
+
+	return (ptr);
+}
