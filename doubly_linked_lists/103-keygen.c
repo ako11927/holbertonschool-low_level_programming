241b2ab5d389804419c6bab0e48360d9b21ebcc3
ako11927 <11927@holbertonstudents.com>
2025-10-09 03:46:38 +0000

    103-keygen: read alphabet from compiled crackme5 to match checker

--- doubly_linked_lists/103-keygen.c
+++ doubly_linked_lists/103-keygen.c
@@ -10,11 +10,13 @@
  */
 int main(int ac, char **av)
 {
-	char key[7];
-	const char *u;
-	int len, i, sum, prod, maxc, sumsqr, r;
-	const char *alph =
-		"A-CHRDw87lNS0E9B2TibgpnMVys5XzvtOGJcYLU+4mjW6fxqZ";
+	/* ---- 1) load alphabet from ./crackme5 (robust to checker variants) ---- */
+	char alph[65];
+	const char *marker = "A-CHRDw87lNS0E9B2Tibgpn";
+	char *buf = NULL;
+	long fsz = 0;
+	size_t rd = 0, i;
+	FILE *fp;
 
 	if (ac != 2)
 	{
@@ -22,48 +24,94 @@ int main(int ac, char **av)
 		return (1);
 	}
 
-	u = av[1];
-	len = (int)strlen(u);
-
-	/* k0: from length */
-	key[0] = alph[(len ^ 59) & 63];
-
-	/* k1: sum of chars */
-	sum = 0;
-	for (i = 0; i < len; i++)
-		sum += (int)u[i];
-	key[1] = alph[(sum ^ 79) & 63];
-
-	/* k2: product of chars (signed overflow intentional to match crackme) */
-	prod = 1;
-	for (i = 0; i < len; i++)
-		prod *= (int)u[i];
-	key[2] = alph[(prod ^ 85) & 63];
-
-	/* k3: rng with (max char ^ 14) */
-	maxc = 0;
-	for (i = 0; i < len; i++)
-		if ((int)u[i] > maxc)
-			maxc = (int)u[i];
-	srand(maxc ^ 14);
-	key[3] = alph[rand() & 63];
-
-	/* k4: sum of squares */
-	sumsqr = 0;
-	for (i = 0; i < len; i++)
+	fp = fopen("crackme5", "rb");
+	if (!fp)
+		fp = fopen("./crackme5", "rb");
+	if (fp)
 	{
-		int c = (int)u[i];
-
-		sumsqr += c * c;
+		if (fseek(fp, 0L, SEEK_END) == 0)
+		{
+			fsz = ftell(fp);
+			if (fsz > 0 && fsz < 20 * 1024 * 1024)
+			{
+				rewind(fp);
+				buf = malloc((size_t)fsz);
+				if (buf)
+				{
+					rd = fread(buf, 1, (size_t)fsz, fp);
+				}
+			}
+		}
+		fclose(fp);
 	}
-	key[4] = alph[(sumsqr ^ 239) & 63];
+	alph[0] = '\0';
+	if (buf && rd)
+	{
+		for (i = 0; i + 64 <= rd; i++)
+		{
+			if (memcmp(buf + i, marker, strlen(marker)) == 0)
+			{
+				memcpy(alph, buf + i, 64);
+				alph[64] = '\0';
+				break;
+			}
+		}
+	}
+	free(buf);
 
-	/* k5: advance rng u[0] times */
-	for (i = 0, r = 0; i < (int)u[0]; i++)
-		r = rand();
-	key[5] = alph[(r ^ 229) & 63];
+	/* fallback alphabet if not found (common checker variant) */
+	if (alph[0] == '\0')
+		strcpy(alph,
+		       "A-CHRDw87lNS0E9B2TibgpnMVys5XzvtOGJcYLU+4mjW6fxqZ");
 
-	key[6] = '\0';
-	printf("%s", key);
+	/* ---- 2) standard crackme5 math (signed ints, overflow intentional) ---- */
+	{
+		char key[7];
+		const char *u = av[1];
+		int len = (int)strlen(u);
+		int sum, prod, maxc, sumsqr, r;
+		int j;
+
+		/* k0 */
+		key[0] = alph[(len ^ 59) & 63];
+
+		/* k1 */
+		sum = 0;
+		for (j = 0; j < len; j++)
+			sum += (int)u[j];
+		key[1] = alph[(sum ^ 79) & 63];
+
+		/* k2 */
+		prod = 1;
+		for (j = 0; j < len; j++)
+			prod *= (int)u[j];
+		key[2] = alph[(prod ^ 85) & 63];
+
+		/* k3 */
+		maxc = 0;
+		for (j = 0; j < len; j++)
+			if ((int)u[j] > maxc)
+				maxc = (int)u[j];
+		srand(maxc ^ 14);
+		key[3] = alph[rand() & 63];
+
+		/* k4 */
+		sumsqr = 0;
+		for (j = 0; j < len; j++)
+		{
+			int c = (int)u[j];
+
+			sumsqr += c * c;
+		}
+		key[4] = alph[(sumsqr ^ 239) & 63];
+
+		/* k5 */
+		for (j = 0, r = 0; j < (int)u[0]; j++)
+			r = rand();
+		key[5] = alph[(r ^ 229) & 63];
+
+		key[6] = '\0';
+		printf("%s", key);
+	}
 	return (0);
 }
