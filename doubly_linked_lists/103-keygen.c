f2f9ed61da639cb84e5ec40b6def9dca1831846b
ako11927 <11927@holbertonstudents.com>
2025-10-09 07:00:44 +0000

    103-keygen: use signed int math (matches crackme5), 6-char, no newline

--- doubly_linked_lists/103-keygen.c
+++ doubly_linked_lists/103-keygen.c
@@ -2,15 +2,14 @@
 #include <stdlib.h>
 #include <string.h>
 
-/* 64-char alphabet extracted from crackme5 */
+/* Alphabet embedded in crackme5 */
 static const char ALPH[] =
 "A-CHRDw87lNS0E9B2TibgpnMVys5XzvtOGJcYLU+4mjW6fxqZ";
 
 int main(int ac, char **av)
 {
-	unsigned int len, i, sum = 0, prod = 1, mx = 0, sq = 0, r = 0;
-	const char *u;
-	char key[7];
+	int len, i, sum = 0, prod = 1, mx, sq = 0, r;
+	char *u, key[7];
 
 	if (ac != 2)
 	{
@@ -19,34 +18,43 @@ int main(int ac, char **av)
 	}
 
 	u = av[1];
-	len = (unsigned int)strlen(u);
+	len = (int)strlen(u);
 
-	key[0] = ALPH[(len ^ 59U) & 63U];
+	/* 1 */
+	key[0] = ALPH[(len ^ 59) & 63];
 
+	/* 2 */
 	for (i = 0; i < len; i++)
-		sum += (unsigned int)u[i];
-	key[1] = ALPH[(sum ^ 79U) & 63U];
+		sum += (int)u[i];
+	key[1] = ALPH[(sum ^ 79) & 63];
 
+	/* 3 */
 	for (i = 0; i < len; i++)
-		prod *= (unsigned int)u[i];
-	key[2] = ALPH[(prod ^ 85U) & 63U];
+		prod *= (int)u[i];
+	key[2] = ALPH[(prod ^ 85) & 63];
 
+	/* 4 */
+	mx = (int)u[0];
 	for (i = 0; i < len; i++)
-		if ((unsigned int)u[i] > mx)
-			mx = (unsigned int)u[i];
-	srand(mx ^ 14U);
-	key[3] = ALPH[rand() & 63U];
+		if ((int)u[i] > mx)
+			mx = (int)u[i];
+	srand(mx ^ 14);
+	key[3] = ALPH[rand() & 63];
 
+	/* 5 */
 	for (i = 0; i < len; i++)
 	{
-		unsigned int c = (unsigned int)u[i];
-		sq += c * c;
-	}
-	key[4] = ALPH[(sq ^ 239U) & 63U];
+		int c = (int)u[i];
 
-	for (i = 0, r = 0; i < (unsigned int)u[0]; i++)
-		r = (unsigned int)rand();
-	key[5] = ALPH[(r ^ 229U) & 63U];
+		sq += (c * c);
+	}
+	key[4] = ALPH[(sq ^ 239) & 63];
+
+	/* 6 */
+	r = 0;
+	for (i = 0; i < (int)u[0]; i++)
+		r = rand();
+	key[5] = ALPH[(r ^ 229) & 63];
 
 	key[6] = '\0';
 	printf("%s", key); /* no newline */
