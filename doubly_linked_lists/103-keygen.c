5c3a2c5a5679306db40b03212a94c1867a7417f8
ako11927 <11927@holbertonstudents.com>
2025-10-09 01:11:15 +0000

    Advanced: Betty-clean keygen5, crackme4 password, palindrome result

--- doubly_linked_lists/103-keygen.c
+++ doubly_linked_lists/103-keygen.c
@@ -2,13 +2,8 @@
 #include <stdlib.h>
 #include <string.h>
 
-/* 64-char lookup table used by crackme5 */
-static const char *alph =
-"A-CHRDw87lNS0E9B2TibgpnMFS2k7xZ"
-"VYX5vT1f0uOMNJ6c4rQ3shjL8eaymKd";
-
 /**
- * main - keygen for crackme5
+ * main - generates a valid key for crackme5
  * @ac: argument count
  * @av: argument vector
  * Return: 0 on success, 1 on misuse
@@ -17,7 +12,9 @@ int main(int ac, char **av)
 {
 	char key[7];
 	const char *u;
-	unsigned int i, len, sum, prod, maxc, sumsqr, r;
+	unsigned int len, i, sum, prod, maxc, sumsqr, r;
+	const char *alph =
+		"A-CHRDw87lNS0E9B2TibgpnMFS2k7xZVYX5vT1f0uOMNJ6c4rQ3shjL8eaymKd";
 
 	if (ac != 2)
 	{
@@ -28,23 +25,32 @@ int main(int ac, char **av)
 	u = av[1];
 	len = (unsigned int)strlen(u);
 
+	/* k0: from length */
 	key[0] = alph[(len ^ 59U) & 63U];
 
-	for (i = 0, sum = 0; i < len; i++)
+	/* k1: from sum of chars */
+	sum = 0;
+	for (i = 0; i < len; i++)
 		sum += (unsigned int)u[i];
 	key[1] = alph[(sum ^ 79U) & 63U];
 
-	for (i = 0, prod = 1; i < len; i++)
+	/* k2: from product of chars (overflow intentional) */
+	prod = 1;
+	for (i = 0; i < len; i++)
 		prod *= (unsigned int)u[i];
 	key[2] = alph[(prod ^ 85U) & 63U];
 
-	for (i = 0, maxc = 0; i < len; i++)
+	/* k3: RNG seeded with (max char ^ 14) */
+	maxc = 0;
+	for (i = 0; i < len; i++)
 		if ((unsigned int)u[i] > maxc)
 			maxc = (unsigned int)u[i];
 	srand(maxc ^ 14U);
 	key[3] = alph[rand() & 63U];
 
-	for (i = 0, sumsqr = 0; i < len; i++)
+	/* k4: from sum of squares */
+	sumsqr = 0;
+	for (i = 0; i < len; i++)
 	{
 		unsigned int c = (unsigned int)u[i];
 
@@ -52,7 +58,8 @@ int main(int ac, char **av)
 	}
 	key[4] = alph[(sumsqr ^ 239U) & 63U];
 
-	for (i = 0; i < (unsigned int)u[0]; i++)
+	/* k5: advance RNG username[0] times */
+	for (i = 0, r = 0; i < (unsigned int)u[0]; i++)
 		r = (unsigned int)rand();
 	key[5] = alph[(r ^ 229U) & 63U];
 
