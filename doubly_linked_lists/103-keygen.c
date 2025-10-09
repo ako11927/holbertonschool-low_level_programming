1cb69b7209a488241c63e0b371e54fbb7d796ec5
ako11927 <11927@holbertonstudents.com>
2025-10-09 04:23:27 +0000

    Betty-clean 103-keygen: tabs, brace style, <=80 cols

--- doubly_linked_lists/103-keygen.c
+++ doubly_linked_lists/103-keygen.c
@@ -6,35 +6,57 @@
 static const char ALPH[] =
 "A-CHRDw87lNS0E9B2TibgpnMVys5XzvtOGJcYLU+4mjW6fxqZ";
 
+/**
+ * main - prints a valid 6-char key for crackme5
+ * @ac: argument count
+ * @av: argument vector
+ * Return: 0 on success, 1 on misuse
+ */
 int main(int ac, char **av)
 {
-    const char *u;
-    unsigned int len, i, sum = 0, prod = 1, mx = 0, sq = 0, r = 0;
-    char key[7];
+	const char *u;
+	unsigned int len, i;
+	unsigned int sum = 0, prod = 1, mx = 0, sq = 0, r = 0;
+	char key[7];
 
-    if (ac != 2) { fprintf(stderr, "Usage: %s username\n", av[0]); return 1; }
-    u = av[1];
-    len = (unsigned int)strlen(u);
+	if (ac != 2)
+	{
+		fprintf(stderr, "Usage: %s username\n", av[0]);
+		return (1);
+	}
 
-    key[0] = ALPH[(len ^ 59U) & 63U];
+	u = av[1];
+	len = (unsigned int)strlen(u);
 
-    for (i = 0; i < len; i++) sum += (unsigned int)u[i];
-    key[1] = ALPH[(sum ^ 79U) & 63U];
+	key[0] = ALPH[(len ^ 59U) & 63U];
 
-    for (i = 0; i < len; i++) prod *= (unsigned int)u[i];
-    key[2] = ALPH[(prod ^ 85U) & 63U];
+	for (i = 0; i < len; i++)
+		sum += (unsigned int)u[i];
+	key[1] = ALPH[(sum ^ 79U) & 63U];
 
-    for (i = 0; i < len; i++) if ((unsigned int)u[i] > mx) mx = (unsigned int)u[i];
-    srand(mx ^ 14U);
-    key[3] = ALPH[rand() & 63U];
+	for (i = 0; i < len; i++)
+		prod *= (unsigned int)u[i];
+	key[2] = ALPH[(prod ^ 85U) & 63U];
 
-    for (i = 0; i < len; i++) { unsigned int c = (unsigned int)u[i]; sq += c * c; }
-    key[4] = ALPH[(sq ^ 239U) & 63U];
+	for (i = 0; i < len; i++)
+		if ((unsigned int)u[i] > mx)
+			mx = (unsigned int)u[i];
+	srand(mx ^ 14U);
+	key[3] = ALPH[rand() & 63U];
 
-    for (i = 0, r = 0; i < (unsigned int)u[0]; i++) r = (unsigned int)rand();
-    key[5] = ALPH[(r ^ 229U) & 63U];
+	for (i = 0; i < len; i++)
+	{
+		unsigned int c = (unsigned int)u[i];
 
-    key[6] = '\0';
-    printf("%s", key); /* no newline */
-    return 0;
+		sq += c * c;
+	}
+	key[4] = ALPH[(sq ^ 239U) & 63U];
+
+	for (i = 0, r = 0; i < (unsigned int)u[0]; i++)
+		r = (unsigned int)rand();
+	key[5] = ALPH[(r ^ 229U) & 63U];
+
+	key[6] = '\0';
+	printf("%s", key);
+	return (0);
 }
