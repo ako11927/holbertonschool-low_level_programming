c009469771efe56d5e5018c82806621891b927a2
ako11927 <11927@helobertonstudent.com>
2025-10-14 14:48:17 +0000

    Cleanup: remove root duplicates; keep canonical files under file_io/

--- file_io/3-cp.c
+++ file_io/3-cp.c
@@ -5,8 +5,11 @@
 #define BUF_SIZE 1024
 
 /**
- * close_checked - close fd and handle errors
- * @fd: file descriptor
+ * close_checked - closes a file descriptor, exiting on failure.
+ * @fd: file descriptor to close
+ *
+ * Exits with code 100 and prints the required error message on stderr
+ * if close(2) fails.
  */
 static void close_checked(int fd)
 {
@@ -18,17 +21,40 @@ static void close_checked(int fd)
 }
 
 /**
- * main - copy the content of a file to another
+ * write_all - writes exactly n bytes or exits with code 99 on failure.
+ * @fd: destination file descriptor
+ * @buf: buffer to write from
+ * @n: number of bytes to write
+ * @name: destination filename for error message
+ */
+static void write_all(int fd, const char *buf, ssize_t n, const char *name)
+{
+	ssize_t off = 0, w;
+
+	while (off < n)
+	{
+		w = write(fd, buf + off, n - off);
+		if (w == -1)
+		{
+			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", name);
+			exit(99);
+		}
+		off += w;
+	}
+}
+
+/**
+ * main - copies the content of one file to another using system calls only.
  * @argc: argument count
- * @argv: argument vector
+ * @argv: argument vector (file_from, file_to)
  *
- * Return: 0 on success, exits with specific codes on errors
+ * Return: 0 on success; exits with codes 97, 98, 99, or 100 on failure.
  */
 int main(int argc, char **argv)
 {
 	int fdf, fdt;
-	ssize_t rbytes, wbytes;
 	char buf[BUF_SIZE];
+	ssize_t r;
 
 	if (argc != 3)
 	{
@@ -36,6 +62,7 @@ int main(int argc, char **argv)
 		exit(97);
 	}
 
+	/* Open source */
 	fdf = open(argv[1], O_RDONLY);
 	if (fdf == -1)
 	{
@@ -43,42 +70,40 @@ int main(int argc, char **argv)
 		exit(98);
 	}
 
+	/* First read BEFORE opening destination so read errors take precedence */
+	r = read(fdf, buf, BUF_SIZE);
+	if (r == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
+		close_checked(fdf);
+		exit(98);
+	}
+
+	/* Open destination only after confirming we can read */
 	fdt = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
 	if (fdt == -1)
 	{
 		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
-		close_checked(fdf); /* will exit 100 on failure */
-		exit(99);           /* not reached, but keeps intent clear */
-	}
-
-	while ((rbytes = read(fdf, buf, BUF_SIZE)) > 0)
-	{
-		ssize_t written = 0;
-
-		while (written < rbytes)
-		{
-			wbytes = write(fdt, buf + written, rbytes - written);
-			if (wbytes == -1)
-			{
-				dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
-				close_checked(fdf);
-				close_checked(fdt);
-				exit(99);
-			}
-			written += wbytes;
-		}
-	}
-
-	if (rbytes == -1)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
 		close_checked(fdf);
-		close_checked(fdt);
-		exit(98);
+		exit(99);
+	}
+
+	/* Write the first chunk already read, then continue the loop */
+	while (r > 0)
+	{
+		write_all(fdt, buf, r, argv[2]);
+
+		r = read(fdf, buf, BUF_SIZE);
+		if (r == -1)
+		{
+			dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
+			close_checked(fdf);
+			close_checked(fdt);
+			exit(98);
+		}
 	}
 
 	close_checked(fdf);
 	close_checked(fdt);
-
 	return (0);
 }
